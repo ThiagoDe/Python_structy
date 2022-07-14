a0b9d130a48a59d9b3e5a82aeb0306acc5da0e0a
ThiagoDe <17tawi@gmail.com>
2022-07-14 14:10:31 -0700

    import

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-def get_initial(name):
-    initial = name[0:1].upper()
-    return initial
-    
+from helpers import *
 
-first_name = input('your name ')
-f_init = get_initial(first_name)
-
-last_name = input('your last name ')
-last_init = get_initial(last_name)
-
-print(f'Your initials {f_init} {last_init} ')
+say_hi()
