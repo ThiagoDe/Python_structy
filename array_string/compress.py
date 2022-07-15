ac097c1c42b71c3b1804890e3d57011fe5442946
ThiagoDe <17tawi@gmail.com>
2022-07-15 11:06:02 -0700

    Compress v2

--- array_string/compress.py
+++ array_string/compress.py
@@ -1,22 +1,39 @@
 def compress(s):
-    s += '!' # add char so the last is different and trigers the else to append 
-    i = 0
-    j = 0
-    restult = []
+    # s += '!' # add char so the last is different and trigers the else to append 
+    # i = 0
+    # j = 0
+    # restult = []
 
-    while j < len(s):
-        if s[i] == s[j]:
-            j += 1
+    # while j < len(s):
+    #     if s[i] == s[j]:
+    #         j += 1
+    #     else:
+    #         num = j - i 
+    #         if num > 1:
+    #             restult.append(str(num))
+    #             restult.append(s[i])
+    #         else:
+    #             restult.append(s[i])
+
+    #         i = j
+    # return ''.join(restult)
+  s += '!'
+  result = []
+  i = 0
+  j = 0
+  while j < len(s):
+    if s[i] == s[j]:
+        j += 1
+    else:
+        num = j - i
+        if num == 1:
+            result.append(s[i])
         else:
-            num = j - i 
-            if num > 1:
-                restult.append(str(num))
-                restult.append(s[i])
-            else:
-                restult.append(s[i])
+            result.append(str(num))
+            result.append(s[i])
+        i = j
 
-            i = j
-    return ''.join(restult)
+  return ''.join(result)
 
 
 print(compress('ccaaatsss'))  # -> '2c3at3s'
