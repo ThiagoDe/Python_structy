922823468ae7f968bf4b4a6244138a301a4f628b
ThiagoDe <17tawi@gmail.com>
2022-07-15 22:53:54 -0700

    Warm up

---
+++ linked_list/warmup.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None 
+
+a = Node('A')
+b = Node('B')
+c = Node('C')
+d = Node('D')
+
+a.next = b
+b.next = c
+c.next = d 
+
+def print_list(head):
+    current = head
+    list = []
+    while current:
+        list.append(current.val)
+        current = current.next 
+
+    return '->'.join(list)
+
+print(print_list(a))
