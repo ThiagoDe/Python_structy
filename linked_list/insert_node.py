a50e1240107b12be519f84d072d76fc9fbcd3b25
ThiagoDe <17tawi@gmail.com>
2022-07-16 17:38:38 -0700

    Insert node v2

--- linked_list/insert_node.py
+++ linked_list/insert_node.py
@@ -11,20 +11,36 @@ c.next = d
 
 # a -> b -> c -> d
 def insert_node(head, value, index):
-    if index == 0:
-        node = Node(value)
-        node.next = head 
-        return node 
+    # if index == 0:
+    #     node = Node(value)
+    #     node.next = head 
+    #     return node 
 
-    current = head 
-    while current:
-        if index == 1:
-            next_node = current.next 
-            current.next = Node(value)
-            current.next.next = next_node
-            return head
-        index -= 1
-        current = current.next 
+    # current = head 
+    # while current:
+    #     if index == 1:
+    #         next_node = current.next 
+    #         current.next = Node(value)
+    #         current.next.next = next_node
+    #         return head
+    #     index -= 1
+    #     current = current.next 
+  if index == 0:
+    new_node = Node(value)
+    new_node.next = head
+    return new_node
+
+  if head is None:
+    return Node
+
+  if index == 1:
+    temp = head.next
+    head.next = Node(value)
+    head.next.next = temp
+    return
+
+  insert_node(head.next, value, index - 1)
+  return head
         
 
 
