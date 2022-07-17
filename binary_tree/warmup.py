995f74515e71e61c3d9c70da7397cc2c193b02cd
ThiagoDe <17tawi@gmail.com>
2022-07-16 18:39:14 -0700

    Warm up binary tree

---
+++ binary_tree/warmup.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+
